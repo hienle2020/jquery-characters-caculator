aa0693122beaab88cc49b9e6e1db48f09182a48c
HienLe <hien.le@kamora.vn>
2021-03-12 10:10:14 +0700

    shorter code

--- dist/js/jquery-characters-caculator.js
+++ dist/js/jquery-characters-caculator.js
@@ -1,48 +1,35 @@
 (function ($) {
 	$.calculate = function (element, target, options) {
-		var defaults = {
+		options = $.extend({
 			limit: 0,
 			whitespaces: true,
-		};
-		var options = $.extend(defaults, options);
+		}, options);
 		var $element = $(element);
 		if (!$element.is('input,select,textarea')) {
 			throw new Error(`Invalid element type: ${element.tagName}!`);
 		}
-		if (!isNaN(target) || target === undefined) {
-			throw new Error(`This field is not true`);
-		}
-		$element.on('paste keyup change', function () {
-			setTimeout(function () { CharCounter($element, target, options.limit, options.whitespaces) }, 10);
+		$element.on('paste keyup keypress change', function () {
+			CharCounter($element, target, options.limit, options.whitespaces)
 		});
 		CharCounter = function (element, target, limit, whitespaces) {
-			let htmlResult, text = element.val(), textLength = text.length;
-			if (whitespaces && limit > 0) {
-				textLength = text.replace(/\s/g, '').length;
-				htmlResult = `<span>${textLength}/${limit}</span>`;
-				if (textLength > limit) {
+			var text = (whitespaces ? element.val().replace(/\s/g, '') : element.val());
+			if (limit > 0) {
+				if (text.length > limit) {
 					let position = limit;
-					let subStrText = text.substr(0, position)
-					while (subStrText.replace(/\s/g, '').length < limit) {
-						subStrText = text.substr(0, position++);
+					let subStrText = element.val().substr(0, position);
+					if(whitespaces){
+						while (subStrText.replace(/\s/g, '').length < limit) {
+							subStrText = element.val().substr(0, position++);
+						}
 					}
 					element.val(subStrText);
-					htmlResult = `<span>${limit}/${limit}</span>`;
-				}
-			} else if (whitespaces) {
-				textLength = text.replace(/\s/g, '').length;
-				htmlResult = `<span>${textLength}</span>`;
-
-			} else if (limit > 0) {
-				htmlResult = `<span>${textLength}/${limit}</span>`;
-				if (textLength > limit) {
-					element.val(text.substr(0, limit));
-					htmlResult = `<span>${limit}/${limit}</span>`;
+					$(target).html(`<span>${limit}/${limit}</span>`);
+				} else {
+					$(target).html(`<span>${text.length}/${limit}</span>`);
 				}
 			} else {
-				htmlResult = `<span>${textLength}</span>`;
+				$(target).html(`<span>${text.length}</span>`);
 			}
-			$(target).html(htmlResult)
 		}
 	}
 	$.fn.calculate = function (target, options) {
